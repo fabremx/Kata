ec79465cf9fca0f4096a65d7261b5434923cd9f1
Maxime Fabre <maxime.fabre@cgi.com>
2019-10-08 15:58:40 +0200

    Update Leap Year

--- src/LeapYear/leapYear.js
+++ src/LeapYear/leapYear.js
@@ -1,10 +1,14 @@
 function isLeapYear(year) {
-  if (year === 1996) {
-    return true;
-  }
-  return false;
+    return (
+        (isDivisibleBy(year, 4) && !isDivisibleBy(year, 100)) || 
+        isDivisibleBy(year, 400)
+    );
+}
+
+function isDivisibleBy(year, number) {
+    return year % number === 0;
 }
 
 module.exports = {
-  isLeapYear
+    isLeapYear
 }
--- src/LeapYear/leapYear.spec.js
+++ src/LeapYear/leapYear.spec.js
@@ -1,34 +1,81 @@
-const { isLeapYear } =  require('../leapYears');
+const { isLeapYear } = require('../leapYears');
 
 it('should return false when year equal to 2001', () => {
-  // Given
-  const year = 2001;
+    // Given
+    const year = 2001;
 
-  // When
-  const result = isLeapYear(year);
+    // When
+    const result = isLeapYear(year);
 
-  // Then
-  expect(result).toBe(false);
+    // Then
+    expect(result).toBe(false);
 })
+
 
 it('should return true when year equal to 1996', () => {
-  // Given
-  const year = 1996;
+    // Given
+    const year = 1996;
 
-  // When
-  const result = isLeapYear(year);
+    // When
+    const result = isLeapYear(year);
 
-  // Then
-  expect(result).toBe(true);
-})
+    // Then
+    expect(result).toBe(true);
+});
+
 
 it('should return true when year equal to 2004', () => {
-  // Given
-  const year = 2004;
+    // Given
+    const year = 2004;
 
-  // When
-  const result = isLeapYear(year);
+    // When
+    const result = isLeapYear(year);
 
-  // Then
-  expect(result).toBe(true);
-})
+    // Then
+    expect(result).toBe(true);
+});
+
+it('should return false when year equal to 1900', () => {
+    // Given
+    const year = 1900;
+
+    // When
+    const result = isLeapYear(year);
+
+    // Then
+    expect(result).toBe(false);
+});
+
+it('should return false when year equal to 1800', () => {
+    // Given
+    const year = 1800;
+
+    // When
+    const result = isLeapYear(year);
+
+    // Then
+    expect(result).toBe(false);
+});
+
+
+it('should return true when year equal to 1600', () => {
+    // Given
+    const year = 1600;
+
+    // When
+    const result = isLeapYear(year);
+
+    // Then
+    expect(result).toBe(true);
+});
+
+it('should return true when year equal to 2000', () => {
+    // Given
+    const year = 2000;
+
+    // When
+    const result = isLeapYear(year);
+
+    // Then
+    expect(result).toBe(true);
+});
