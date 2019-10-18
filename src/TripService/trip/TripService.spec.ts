9d957ac5a988a8e9c3db6708f6faafffba659e69
Maxime Fabre <maxime.fabre@cgi.com>
2019-10-18 12:05:01 +0200

    Trip service: cover code with unit tests

--- src/TripService/trip/TripService.spec.ts
+++ src/TripService/trip/TripService.spec.ts
@@ -3,15 +3,17 @@ import UserSession from '../user/UserSession';
 import User from '../user/User';
 import Trip from './Trip';
 import UserNotLoggedInException from '../exception/UserNotLoggedInException';
+import TripDAO from './TripDAO';
 
 describe('TripService - getTripsByUser', () => {
     const NOT_LOGGED_USER = null;
     const UNUSED_USER = null;
 
-    const LOGGED_USER = new User();
+    const LOGGED_IN_USER = new User();
     const ANOTHER_USER = new User();
 
     const TO_BRAZIL = new Trip();
+    const TO_LONDON =  new Trip();
 
     it('should throw exception when user is not logged', () => {
         // Given
@@ -29,20 +31,39 @@ describe('TripService - getTripsByUser', () => {
         }
     });
 
-    it('should return any trips when user is not firend with loggedUser', () => {
+    it('should return any trips when users are not friends', () => {
         // Given
         const tripService = new TripService();
-        UserSession.getLoggedUser = jest.fn().mockReturnValue(LOGGED_USER);
+        UserSession.getLoggedUser = jest.fn().mockReturnValue(LOGGED_IN_USER);
         
         const friend: User = new User();
         friend.addFriend(ANOTHER_USER);
         friend.addTrip(TO_BRAZIL);
-        friend.getFriends = jest.fn().mockReturnValue(friend.getFriends);
 
         // When
         const friendTrips: Trip[]  = tripService.getTripsByUser(friend)
 
         // Then
         expect(friendTrips).toEqual([]);
-    })
+    });
+
+    it('should return friend trips when users are friends', () => {
+        // Given
+        const tripService = new TripService();
+        const friend: User = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addFriend(LOGGED_IN_USER);
+        friend.addTrip(TO_BRAZIL);
+        friend.addTrip(TO_LONDON);
+
+        UserSession.getLoggedUser = jest.fn().mockReturnValue(LOGGED_IN_USER);
+        TripDAO.findTripsByUser = jest.fn().mockReturnValue(friend.getTrips());
+
+        // When
+        const friendTrips: Trip[]  = tripService.getTripsByUser(friend)
+
+        // Then
+        const expectedFriendTrips = friend.getTrips();
+        expect(friendTrips).toEqual(expectedFriendTrips);
+    });
 });
