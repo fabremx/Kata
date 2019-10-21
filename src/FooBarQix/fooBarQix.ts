3377fffcb4555e83df08ae1c4f489ff1ebf22677
fabremx <fabre.m96@gmail.com>
2019-10-22 00:00:30 +0200

    FooBarQix: pass code from js to ts and finish resultByOccurence method

--- src/FooBarQix/fooBarQix.ts
+++ src/FooBarQix/fooBarQix.ts
@@ -1,70 +1,79 @@
-const dividedBy3 = 'Foo';
-const dividedBy5 = 'Bar';
-const dividedBy7 = 'Qix';
+export default class FooBarQix {
+    foo = 'Foo';
+    bar = 'Bar';
+    qix = 'Qix';
 
-function resultByOccurence(number) {
-    const numberArray = number.toString().split('').map(Number);
-    let result = '';
+    public fooBarQix(number: number): string|number {
+        let result = this.resultByDivision(number);
+        if (number === 3) {
+            result += this.resultByOccurence(number);
+        }
+    
+        return result;
+    }
 
-    for (let i = 0; i < numberArray.length; i++) {
-        if (numberArray[i] === 3) {
-            result += dividedBy3;
+    public resultByOccurence(number: number): string {
+        const listOfDigit: number[] = this.splitNumber(number);
+        return this.resultFromDigitOccurence(listOfDigit);
+    }
+
+    private resultFromDigitOccurence(listOfDigit: number[]): string {
+        return listOfDigit
+            .map((digit) => this.resultBy(digit))
+            .join("");
+    }
+
+    private resultBy(digit: number): string {
+        switch (digit) {
+            case 3:
+                return this.foo;
+            case 5:
+                return this.bar;
+            case 7:
+                return this.qix;
         }
     }
 
-    return result;
-}
-
-function fooBarQix(number) {
-    let result = resultByDivision(number);
-    if (number === 3) {
-        result += resultByOccurence(number);
+    private splitNumber(number: number): number[] {
+        return number.toString().split('').map(Number);
     }
 
-    return result;
-}
+    public resultByDivision(number: number): string|number {
+        if (this.isDivisibleBy3(number) && this.isDivisibleBy5(number) && this.isDivisibleBy7(number)) {
+            return this.foo + this.bar + this.qix;
+        }
+        else if (this.isDivisibleBy3(number) && this.isDivisibleBy7(number)) {
+            return this.foo + this.qix;
+        }
+        else if (this.isDivisibleBy5(number) && this.isDivisibleBy7(number)) {
+            return this.bar + this.qix;
+        }
+        else if (this.isDivisibleBy3(number) && this.isDivisibleBy5(number)) {
+            return this.foo + this.bar;
+        }
+        else if (this.isDivisibleBy3(number)) {
+            return this.foo;
+        }
+        else if (this.isDivisibleBy5(number)) {
+            return this.bar;
+        }
+        else if (this.isDivisibleBy7(number)) {
+            return this.qix;
+        }
+        else {
+            return number;
+        }
+    }
 
-function resultByDivision(number) {
-    if (isDivisibleBy3(number) && isDivisibleBy5(number) && isDivisibleBy7(number)) {
-        return dividedBy3 + dividedBy5 + dividedBy7;
+    private isDivisibleBy7(number: number): boolean {
+        return number % 7 === 0;
     }
-    else if (isDivisibleBy3(number) && isDivisibleBy7(number)) {
-        return dividedBy3 + dividedBy7;
+    
+    private isDivisibleBy3(number: number): boolean {
+        return number % 3 === 0;
     }
-    else if (isDivisibleBy5(number) && isDivisibleBy7(number)) {
-        return dividedBy5 + dividedBy7;
+    
+    private isDivisibleBy5(number: number): boolean {
+        return number % 5 === 0;
     }
-    else if (isDivisibleBy3(number) && isDivisibleBy5(number)) {
-        return dividedBy3 + dividedBy5;
-    }
-    else if (isDivisibleBy3(number)) {
-        return dividedBy3;
-    }
-    else if (isDivisibleBy5(number)) {
-        return dividedBy5;
-    }
-    else if (isDivisibleBy7(number)) {
-        return dividedBy7;
-    }
-    else {
-        return number;
-    }
-}
-
-function isDivisibleBy7(number) {
-    return number % 7 === 0;
-}
-
-function isDivisibleBy3(number) {
-    return number % 3 === 0;
-}
-
-function isDivisibleBy5(number) {
-    return number % 5 === 0;
-}
-
-export {
-    resultByDivision,
-    fooBarQix,
-    resultByOccurence
 }
